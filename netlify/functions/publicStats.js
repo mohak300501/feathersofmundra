7e8e4c6e037270029128cbe6832a3e50e092969b
mohak300501 <mohak_k@ph.iitr.ac.in>
2026-07-10 01:39:13 +0530

    refactor: remove user leaderboards and replace with total user count in public stats function

--- netlify/functions/publicStats.js
+++ netlify/functions/publicStats.js
@@ -24,63 +24,8 @@ exports.handler = async (event, context) => {
     // 2. Total bird species
     const totalBirds = await db.collection('birds').countDocuments();
 
-    // 3. Leaderboard for Photo Count
-    const photoLeaders = await db.collection('photos').aggregate([
-      {
-        $group: {
-          _id: '$uploadedBy',
-          count: { $sum: 1 }
-        }
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: '_id',
-          foreignField: 'uid',
-          as: 'user'
-        }
-      },
-      { $unwind: '$user' },
-      {
-        $project: {
-          _id: 0,
-          userId: '$_id',
-          username: '$user.username',
-          count: 1
-        }
-      },
-      { $sort: { count: -1 } },
-      { $limit: 10 }
-    ]).toArray();
-
-    // 4. Leaderboard for Bird Species Uploaded
-    const birdLeaders = await db.collection('birds').aggregate([
-      {
-        $group: {
-          _id: '$addedBy',
-          count: { $sum: 1 }
-        }
-      },
-      {
-        $lookup: {
-          from: 'users',
-          localField: '_id',
-          foreignField: 'uid',
-          as: 'user'
-        }
-      },
-      { $unwind: '$user' },
-      {
-        $project: {
-          _id: 0,
-          userId: '$_id',
-          username: '$user.username',
-          count: 1
-        }
-      },
-      { $sort: { count: -1 } },
-      { $limit: 10 }
-    ]).toArray();
+    // 3. Total users
+    const totalUsers = await db.collection('users').countDocuments();
 
     return {
       statusCode: 200,
@@ -88,8 +33,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({
         totalPhotos,
         totalBirds,
-        topPhotographers: photoLeaders,
-        topContributors: birdLeaders
+        totalUsers
       }),
     };
 
